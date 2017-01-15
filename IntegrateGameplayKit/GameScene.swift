518b9ed0652dd8f5ecf4eb7561099afddc3381a5
tnantoka <tnantoka@bornneet.com>
2017-01-15 20:17:24 +0900

    checked

--- IntegrateGameplayKit/GameScene.swift
+++ IntegrateGameplayKit/GameScene.swift
@@ -11,10 +11,16 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
-    override func didMove(to view: SKView) {
+    override func sceneDidLoad() {
+
+        self.lastUpdateTime = 0
         
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
@@ -85,5 +91,20 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
     }
 }
